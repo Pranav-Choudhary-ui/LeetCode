e44892d3145eb08cc4951974fa14083d8fb91e6e
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-07-21 09:05:23 +0530

    Create 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions

---
+++ 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions/1724-CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,12 @@
+-- Last updated: 21/07/2026, 09:05:22
+SELECT 
+    v.customer_id, COUNT(v.visit_id) AS count_no_trans 
+FROM 
+    Visits v 
+LEFT JOIN 
+    Transactions t 
+    ON v.visit_id = t.visit_id
+WHERE 
+    transaction_id IS NULL
+GROUP BY 
+    customer_id;
