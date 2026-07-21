41152fe7e58607640e505f16b625c869c65ea2df
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-07-21 09:08:03 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.cpp
@@ -0,0 +1,16 @@
+// Last updated: 21/07/2026, 09:08:03
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        double half = myPow(x, n/2);
+        if(n%2==0){
+            return half*half;
+        }
+        else{
+            return (n>0) ? half*half*x : half*half/x;
+        }
+    }
+};
