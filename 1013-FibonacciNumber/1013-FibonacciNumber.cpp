6277d559d7843407348ac355ab626deb8d059e1b
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-07-21 09:05:50 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,12 @@
+// Last updated: 21/07/2026, 09:05:50
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+
+        return (fib(n-1) + fib(n-2));
+        
+    }
+};
