db078f1f7e4dc55c7e933ed2c9ca0a463f603c7d
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-07-22 12:14:06 +0530

    Create 207-CourseSchedule

---
+++ 207-CourseSchedule/207-CourseSchedule.cpp
@@ -0,0 +1,38 @@
+// Last updated: 22/07/2026, 12:14:06
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        for(int i=0;i<prerequisites.size();i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(int nei:adj[node]){
+                indegree[nei]--;
+                if(indegree[nei] == 0){
+                    q.push(nei);
+                }
+            }
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i] != 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
