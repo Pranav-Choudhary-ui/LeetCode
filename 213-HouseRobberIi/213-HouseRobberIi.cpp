d2e70822d3f27934f6094a368a2ef9791bff584c
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-08-12 22:27:47 +0530

    Create 213-HouseRobberIi

---
+++ 213-HouseRobberIi/213-HouseRobberIi.cpp
@@ -0,0 +1,35 @@
+// Last updated: 12/08/2026, 22:27:46
+class Solution {
+public:
+    int dp[101];
+    int solve(vector<int>& nums, int n){
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        if(n == 0){
+            return dp[n] = nums[n];
+        }
+        if(n == 1){
+            return dp[n] = max(nums[0], nums[1]);
+        }
+
+        int take = nums[n] + solve(nums, n-2);
+        int leave = solve(nums, n-1);
+        
+        return dp[n] = max(take, leave);
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1){
+            return nums[0];
+        }
+        memset(dp, -1, sizeof(dp));
+        vector<int> a(nums.begin()+1, nums.end());
+        vector<int> b(nums.begin(), nums.end()-1);
+
+        int x = solve(a, a.size()-1);
+        memset(dp, -1, sizeof(dp));
+        int y = solve(b, b.size()-1);
+
+        return max(x, y);
+    }
+};
