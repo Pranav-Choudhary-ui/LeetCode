f4150a95564213dab611a9a67ad6d4788376c6b4
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-08-20 21:40:00 +0530

    File updated at 20/08/2026, 21:39:59

--- 162-FindPeakElement/162-FindPeakElement.cpp
+++ 162-FindPeakElement/162-FindPeakElement.cpp
@@ -1,19 +1,20 @@
-// Last updated: 21/07/2026, 09:07:19
-class Solution {
-public:
-    int findPeakElement(vector<int>& nums) {
-        int l = 0, r = nums.size() - 1;
-
-        while(l < r){
-            int mid = l + (r - l) / 2;
-
-            if(nums[mid] < nums[mid+1]){
-                l = mid + 1;
-            }
-            else
-                r = mid;   
-        }
-        return l;
-    }
-};
-
+// Last updated: 20/08/2026, 21:39:59
+1class Solution {
+2public:
+3    int findPeakElement(vector<int>& nums) {
+4        int l = 0;
+5        int size = nums.size();
+6        int h = size-1;
+7
+8        while(l<h){
+9            int mid = l+(h-l)/2;
+10            if(mid+1 < size && nums[mid] > nums[mid+1]){
+11                h = mid;
+12            }
+13            else if(mid+1 < size && nums[mid] < nums[mid+1]){
+14                l = mid+1;
+15            }
+16        }
+17        return l;
+18    }
+19};
