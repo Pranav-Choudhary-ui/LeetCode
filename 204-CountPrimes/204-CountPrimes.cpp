cd23a82fe9a2487bf674cd4f203671f4060c1b33
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-08-21 21:08:48 +0530

    File updated at 21/08/2026, 21:08:48

--- 204-CountPrimes/204-CountPrimes.cpp
+++ 204-CountPrimes/204-CountPrimes.cpp
@@ -1,20 +1,20 @@
-// Last updated: 21/07/2026, 09:07:08
-class Solution {
-public:
-    int countPrimes(int n) {
-        vector<bool> prime(n+1,true);
-        prime[0] = false;
-        prime[1] = false;
-        int c=0;
-        for(int i=2;i<n;i++){
-            if(prime[i]==true){
-                c++;
-                for(int j=2*i;j<n;j=j+i){
-                prime[j]=false;
-            }
-            
-            }
-        }
-        return c;
-    }
-};
+// Last updated: 21/08/2026, 21:08:48
+1class Solution {
+2public:
+3    int countPrimes(int n) {
+4        vector<bool> prime(n+1,true);
+5        prime[0] = false;
+6        prime[1] = false;
+7        int c=0;
+8        for(int i=2;i<n;i++){
+9            if(prime[i]==true){
+10                c++;
+11                for(int j=2*i;j<n;j=j+i){
+12                prime[j]=false;
+13            }
+14            
+15            }
+16        }
+17        return c;
+18    }
+19};
