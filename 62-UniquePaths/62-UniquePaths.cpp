2835ea76a5b5e3be624ac702b15d00d75878f202
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-08-24 20:58:48 +0530

    File updated at 24/08/2026, 20:58:47

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,9 +1,9 @@
-// Last updated: 24/08/2026, 20:40:50
+// Last updated: 24/08/2026, 20:58:47
 1class Solution {
 2public:
 3    int dp[101][101];
 4    int possiblePaths(int m, int n){
-5        if(m == 0 && n == 0){
+5        if(m == 0 || n == 0){
 6            return 1;
 7        }
 8
@@ -11,13 +11,13 @@
 10            return dp[m][n];
 11        }
 12
-13        if(m == 0){
-14            return dp[m][n] = possiblePaths(m, n-1);
-15        }
+13        // if(m == 1){
+14        //     return dp[m][n] = possiblePaths(m, n-1);
+15        // }
 16
-17        if(n == 0){
-18            return dp[m][n] = possiblePaths(m-1, n);
-19        }
+17        // if(n == 1){
+18        //     return dp[m][n] = possiblePaths(m-1, n);
+19        // }
 20
 21        return dp[m][n] = possiblePaths(m, n-1) + possiblePaths(m-1, n);
 22
