b0235a1de09c45ba8f19d189a570d4a9b363007b
Pranav Choudhary <132654519+Pranav-Choudhary-ui@users.noreply.github.com>
2026-08-25 17:40:02 +0530

    File updated at 25/08/2026, 17:40:02

--- 322-CoinChange/322-CoinChange.cpp
+++ 322-CoinChange/322-CoinChange.cpp
@@ -1,18 +1,32 @@
-// Last updated: 21/07/2026, 09:06:45
-class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        const int INF = 1e9 + 7;
-        vector<int> dp(amount+1, INF);
-        dp[0] = 0;
-        for(int i=0;i<coins.size();i++){
-            for(int j=coins[i];j<=amount;j++){
-                dp[j] = min(dp[j], 1 + dp[j-coins[i]]);
-            }
-        }
-        if(dp[amount] == INF){
-            return -1;
-        }
-        return dp[amount];
-    }
-};
+// Last updated: 25/08/2026, 17:40:02
+1class Solution {
+2public:
+3    int dp[13][10001];
+4    int solve(vector<int>& coins, int n, int amount) {
+5        if (amount == 0)
+6            return 0;
+7
+8        if (n == 0)
+9            return 1e9;
+10
+11        if (dp[n][amount] != -1)
+12            return dp[n][amount];
+13
+14        int notTake = solve(coins, n - 1, amount);
+15        int take = 1e9;
+16
+17        if (coins[n - 1] <= amount) {
+18            take = 1 + solve(coins, n, amount - coins[n - 1]);
+19        }
+20
+21        return dp[n][amount] = min(take, notTake);
+22    }
+23
+24    int coinChange(vector<int>& coins, int amount) {
+25        memset(dp, -1, sizeof(dp));
+26
+27        int ans = solve(coins, coins.size(), amount);
+28
+29        return ans >= 1e9 ? -1 : ans;
+30    }
+31};
